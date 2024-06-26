8f407cd7dece603422ab9c9f07ec116ffb55eed6
Charly <cardan1990@hotmail.com>
2024-06-26 17:12:13 +0200

    added css classes to html elements

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,10 +1,12 @@
 const Tour = ({ id, image, name, info, price }) => {
   return (
     <article className='single-tour'>
-      <img src={image} alt={name} />
-      <span>${price}</span>
-      <h2>{name}</h2>
-      <p>{info}</p>
+      <img src={image} alt={name} className='img' />
+      <span className='tour-price'>${price}</span>
+      <div className='tour-info'>
+        <h5>{name}</h5>
+        <p>{info}</p>
+      </div>
       <button className='btn'>Not interested</button>
     </article>
   )
