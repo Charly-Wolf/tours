02cefd9aee99f84377837d3024096e266d21ead5
Charly <cardan1990@hotmail.com>
2024-06-26 16:52:18 +0200

    implemented fetch tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-const url = 'https://www.course-api.com/react-tours-project';
+import { useEffect, useState } from 'react'
+import Loading from './components/Loading'
+
+const url = 'https://www.course-api.com/react-tours-project'
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
-};
-export default App;
+  const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  const fetchTours = async () => {
+    try {
+      const resp = await fetch(url)
+      const data = await resp.json()
+      setTours(data)
+      console.log(tours)
+
+      setIsError(false)
+    } catch (error) {
+      console.log(error)
+      setIsError(true)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (isLoading) return <Loading />
+  if (isError) return <h1>Error</h1>
+
+  return <h2>On Tours</h2>
+}
+export default App
