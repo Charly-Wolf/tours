503d84c11dea49733b09c0308e44b46c334ed9e0
Charly <cardan1990@hotmail.com>
2024-06-27 09:48:54 +0200

    Added refetch tours button

--- src/App.jsx
+++ src/App.jsx
@@ -45,9 +45,27 @@ const App = () => {
       </main>
     )
 
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>No Tours</h2>
+          <button
+            type='button'
+            style={{ marginTop: '2rem' }}
+            onClick={() => fetchTours()}
+            className='btn'
+          >
+            Refetch
+          </button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <main>
-      <Tours tours={tours} removeTour={removeTour}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
